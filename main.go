27bde2161f4017e50b3ac06ecb692342239c65c1
Qesait <kuzva2003@gmail.com>
2024-02-19 20:06:12 +0300

    string -> bytes

--- main.go
+++ main.go
@@ -1,92 +1,96 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"time"
+    "bytes"
+    "encoding/json"
+    "errors"
+    "net/http"
+    "time"
+
+    "github.com/gin-gonic/gin"
 )
 
-type Message struct {
-	Text string    `json:"text"`
-	Time time.Time `json:"time"`
-}
-
 func main() {
-	r := gin.Default()
+    r := gin.Default()
 
-	r.POST("/split", split)
+    r.POST("/segmentation", segmentation)
 
-	r.Run(":8080")
+    r.Run(":8080")
 }
 
-func split(c *gin.Context) {
-	var message Message
-	if err := c.ShouldBind(&message); err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
-		return
-	}
+type Message struct {
+    Text []byte   `json:"text"`
+    Time time.Time `json:"time"`
+}
 
-	portions := splitData(message.Text, 120)
+func segmentation(c *gin.Context) {
+    var message Message
+    if err := c.ShouldBindJSON(&message); err != nil {
+        c.AbortWithError(http.StatusBadRequest, err)
+        return
+    }
 
-	for _, portion := range portions {
-		segment := Segment{
-			Data:  portion,
-			Time:  message.Time,
-			Count: len(portions),
-		}
-		send(segment)
-	}
+    portions := split(message.Text, 120)
 
-	c.JSON(http.StatusOK, gin.H{"message": "Data processed and sent"})
+    for _, portion := range portions {
+        segment := Segment{
+            Data:  portion,
+            Time:  message.Time,
+            Count: len(portions),
+        }
+        if err := send(segment); err != nil {
+            c.AbortWithError(http.StatusInternalServerError, err)
+            return
+        }
+    }
+
+    c.Status(http.StatusOK)
 }
 
 type Segment struct {
-	Data  string    `json:"data"`
-	Time  time.Time `json:"time"`
-	Count int       `json:"count"`
+    Data  []byte    `json:"data"`
+    Time  time.Time `json:"time"`
+    Count int       `json:"count"`
 }
 
 func send(segment Segment) error {
-	url := "http://localhost:8000/api/delivery/"
+    url := "http://localhost:8000/api/delivery/"
 
-	jsonData, err := json.Marshal(segment)
-	if err != nil {
-		return err
-	}
+    jsonData, err := json.Marshal(segment)
+    if err != nil {
+        return err
+    }
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+    if err != nil {
+        return err
+    }
+    req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
+    client := &http.Client{}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+    resp, err := client.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return errors.New("Unexpected response status: " + resp.Status)
-	}
-	return nil
+    if resp.StatusCode != http.StatusOK {
+        return errors.New("Unexpected response status: " + resp.Status)
+    }
+    return nil
 }
 
-func splitData(data string, chunkSize int) []string {
-	var portions []string
+func split(data []byte, chunkSize int) [][]byte {
+    var portions [][]byte
 
-	for i := 0; i < len(data); i += chunkSize {
-		end := i + chunkSize
-		if end > len(data) {
-			end = len(data)
-		}
-		portions = append(portions, data[i:end])
-	}
+    for i := 0; i < len(data); i += chunkSize {
+        end := i + chunkSize
+        if end > len(data) {
+            end = len(data)
+        }
+        portions = append(portions, data[i:end])
+    }
 
-	return portions
+    return portions
 }
